fd101207dfd8a4b749181c2de4f184286fa3b6b4
INEEDSSD <1256123798@qq.com>
2021-07-27 00:30:03 +0800

    * update the model's signal and the vector3 of model to update the position
                 two method to finish the position of model(positon and velocity)
    * update the animator contoller of the model
                modify the parmater "forward" of the contorller
                use 1D blend tree of Animation

---
+++ Assets/Scripts/ActorController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorController : MonoBehaviour
+{
+    public GameObject model;    // 引用模型对象
+    public PlayerInput playerInput;    // 引用输入模块
+    public float walkSpeed; // 移动速度用来设置与动画的播放速度匹配
+    [SerializeField]  // 序列化下,能够序列化的只能是unity的组件内容
+    private Animator anim;  // 引用动画控制器
+    private Rigidbody rigid;    // 引用刚体
+    // 移动位置
+    private Vector3 MovingVec;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        // 加载动画控制器
+        anim = model.GetComponent<Animator>();
+        // 加载输入模块
+        playerInput = this.GetComponent<PlayerInput>();
+        // 加载刚体
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 设置动画控制器的forward变量，出入Dup信号
+        //                    y
+        //                    |
+        //                    |
+        //                    |
+        //                    |
+        //                    |                             
+        //---------------------------------------x
+        //                    |
+        //                    |
+        //          p(0.3,0.4)|
+        //                    |
+        //                    |
+        //                    |
+        anim.SetFloat("forward", playerInput.DMag);
+        // 处理因为松开按键导致的模型朝向突变的问题
+        if (playerInput.DMag > 0.1f)
+        {
+            // 转换模型的朝向 标量 * 单位向量, 两个向量的和
+            model.transform.forward = playerInput.DVec;
+        }
+
+        MovingVec = playerInput.DMag * model.transform.forward * walkSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        // 进行物理计算
+        //rigid.position = MovingVec * Time.fixedDeltaTime + rigid.position;
+
+        // 也可以直接使用Velocity, 刷新y轴上的速度与坐标
+        rigid.velocity = new Vector3(MovingVec.x, rigid.velocity.y, MovingVec.z);
+    }
+
+    private void LateUpdate()
+    {
+       
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+public class PlayerInput : MonoBehaviour
+{
+
+    // variable
+    public string keyUp;   
+    public string keyDown;
+    public string keyLeft;
+    public string keyRight;
+
+    // flag
+    // 是否开启此模块
+    public bool PlayerInputEnabled = true;
+    
+    // signal
+    public float DUp;   // DirectionUp
+    public float DRight;    // DirrectionRight
+
+    public float DMag; // DirectionMagnetic
+    public Vector3 DVec; // DirectionVector
+
+    private float targetDUp;
+    private float targetDRight;
+
+    private float velocityDUp;
+
+    private float VelocityDRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 按键输入
+        targetDUp = (Input.GetKey(keyUp) ? 1.0f : 0) - (Input.GetKey(keyDown) ? 1.0f : 0);
+        targetDRight = (Input.GetKey(keyRight) ? 1.0f : 0) - (Input.GetKey(keyLeft) ? 1.0f : 0);
+
+        if (!PlayerInputEnabled)
+        {
+            targetDUp = 0;
+            targetDRight = 0;
+        }
+        
+        // 实现信号量的平滑过渡
+        DUp = Mathf.SmoothDamp(DUp, targetDUp, ref velocityDUp, 0.1f);
+        DRight = Mathf.SmoothDamp(DRight, targetDRight, ref VelocityDRight, 0.1f);
+        DMag = Mathf.Sqrt(DUp * DUp + DRight * DRight) > 1.0f ? 1.0f : Mathf.Sqrt(DUp * DUp + DRight * DRight);
+        DVec = DRight * transform.right + DUp * transform.forward;
+
+    }
+}
